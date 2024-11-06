2b0ce52cb96e71e14dd234ff22f8534981322eb2
Vincent Domaschio <vdomasch@z2r6p2.42lyon.fr>
2024-11-06 10:16:52 +0100

    norminette done; delete fps 'not allowed function'

--- srcs/raycasting/draw.c
+++ srcs/raycasting/draw.c
@@ -6,7 +6,7 @@
 /*   By: vdomasch <vdomasch@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 12:33:26 by vdomasch          #+#    #+#             */
-/*   Updated: 2024/10/26 14:47:07 by vdomasch         ###   ########.fr       */
+/*   Updated: 2024/11/06 10:15:39 by vdomasch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ static int	find_side(t_raycast *raycast)
 	}
 }
 
-void	put_textures(t_data *data, t_raycast *ray, int x, int y)
+/*void	put_textures(t_data *data, t_raycast *ray, int x, int y)
 {
 	int		color;
 	int		scaling;
@@ -70,33 +70,16 @@ void	put_textures(t_data *data, t_raycast *ray, int x, int y)
 		tex_y = data->textures.images[find_side(ray)].height - 1;
 	color = get_pixel(data->textures.images, tex_x, tex_y);
 	my_mlx_pixel_put(&data->mlx.img, x, y, color);
-}
+}*/
 
-void	draw(t_data *d, t_raycast *ray, int x)
+void	draw_loop(t_data *d, t_raycast *ray, int img_width, int x)
 {
 	t_image			*current_tex;
 	unsigned int	*img_data;
-	int				img_width;
 	int				y;
 
-	ray->tex_num = find_side(ray);
 	current_tex = &d->textures.images[ray->tex_num];
-	if (ray->side == 0)
-		ray->wall_x = d->player.pos_y + ray->perp_wall_dist * ray->ray_dir_y;
-	else
-		ray->wall_x = d->player.pos_x + ray->perp_wall_dist * ray->ray_dir_x;
-	ray->wall_x -= floor(ray->wall_x);
-	ray->tex_x = (int)(ray->wall_x * current_tex->width);
-	if ((ray->side == 0 && ray->ray_dir_x < 0)
-		|| (ray->side == 1 && ray->ray_dir_y > 0))
-		ray->tex_x = current_tex->width - ray->tex_x - 1;
-	// Pre-calculate texture step and position
-	ray->step = (double)current_tex->height / ray->line_height;
-	ray->tex_pos = (ray->draw_start - HEIGHT / 2 + ray->line_height / 2)
-		* ray->step;
-	// Draw optimization using direct memory access
 	img_data = (unsigned int *)d->mlx.img.addr;
-	img_width = d->mlx.img.line_length / (d->mlx.img.bits_per_pixel / 8);
 	y = 0;
 	while (y < ray->draw_start)
 		img_data[(y++) * img_width + x] = d->textures.ceiling_color;
@@ -109,4 +92,27 @@ void	draw(t_data *d, t_raycast *ray, int x)
 	}
 	while (y < HEIGHT)
 		img_data[(y++) * img_width + x] = d->textures.floor_color;
+}
+
+void	draw(t_data *d, t_raycast *ray, int x)
+{
+	t_image			*current_tex;
+	int				img_width;
+
+	ray->tex_num = find_side(ray);
+	current_tex = &d->textures.images[ray->tex_num];
+	if (ray->side == 0)
+		ray->wall_x = d->player.pos_y + ray->perp_wall_dist * ray->ray_dir_y;
+	else
+		ray->wall_x = d->player.pos_x + ray->perp_wall_dist * ray->ray_dir_x;
+	ray->wall_x -= floor(ray->wall_x);
+	ray->tex_x = (int)(ray->wall_x * current_tex->width);
+	if ((ray->side == 0 && ray->ray_dir_x < 0)
+		|| (ray->side == 1 && ray->ray_dir_y > 0))
+		ray->tex_x = current_tex->width - ray->tex_x - 1;
+	ray->step = (double)current_tex->height / ray->line_height;
+	ray->tex_pos = (ray->draw_start - HEIGHT / 2 + ray->line_height / 2)
+		* ray->step;
+	img_width = d->mlx.img.line_length / (d->mlx.img.bits_per_pixel / 8);
+	draw_loop(d, ray, img_width, x);
 }
